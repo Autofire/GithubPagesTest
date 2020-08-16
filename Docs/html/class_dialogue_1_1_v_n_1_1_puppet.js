d89d4e2e9968bad8dd1f42ea47ad412dbc10268b
Autofire <Autofire@users.noreply.github.com>
2020-08-15 20:44:41 -0700

    Testing stuff.

---
+++ Docs/html/class_dialogue_1_1_v_n_1_1_puppet.js
@@ -0,0 +1,8 @@
+var class_dialogue_1_1_v_n_1_1_puppet =
+[
+    [ "SetMovementDestination", "class_dialogue_1_1_v_n_1_1_puppet.html#ae5107c7b64933f9f360be42881221046", null ],
+    [ "SetMovementDestination", "class_dialogue_1_1_v_n_1_1_puppet.html#a047cee3d9a8e36bf527576fa62be357f", null ],
+    [ "Warp", "class_dialogue_1_1_v_n_1_1_puppet.html#a5f924ee7d9542bf89220a40315071350", null ],
+    [ "Warp", "class_dialogue_1_1_v_n_1_1_puppet.html#a311fa118cf2ae5658eb2d3be857e9477", null ],
+    [ "movementSpeed", "class_dialogue_1_1_v_n_1_1_puppet.html#a398a038d89d57524a747ec2c37b6eb52", null ]
+];
---
+++ Docs/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['textspeed',['textSpeed',['../class_dialogue_1_1_nijito_dialogue_u_i.html#a8b40e1bc19c88aae1e066e137598e6e5',1,'Dialogue::NijitoDialogueUI']]]
+];
